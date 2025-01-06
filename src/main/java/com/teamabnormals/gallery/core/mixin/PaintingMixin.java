46a22ecee92ecfb9c9f33e54706f260d717a2b7e
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-01-06 15:38:58 -0600

    Using pick block on a painting now gives the variant

--- src/main/java/com/teamabnormals/gallery/core/mixin/PaintingMixin.java
+++ src/main/java/com/teamabnormals/gallery/core/mixin/PaintingMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.world.entity.decoration.PaintingVariant;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.Level;
@@ -17,7 +18,9 @@ import net.minecraftforge.common.Tags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import javax.annotation.Nullable;
 
@@ -33,16 +36,25 @@ public abstract class PaintingMixin extends HangingEntity {
 
 	@Redirect(method = "dropItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/decoration/Painting;spawnAtLocation(Lnet/minecraft/world/level/ItemLike;)Lnet/minecraft/world/entity/item/ItemEntity;"))
 	public ItemEntity spawnAtLocation(Painting painting, ItemLike item, @Nullable Entity entity) {
+		ItemStack stack = new ItemStack(item);
 		if (entity instanceof Player player) {
 			ItemStack tool = player.getItemInHand(player.getUsedItemHand());
 			if (tool.is(Tags.Items.SHEARS) || EnchantmentHelper.hasSilkTouch(tool)) {
-				ItemStack stack = new ItemStack(item);
-				CompoundTag compoundtag = stack.getOrCreateTagElement("EntityTag");
-				Painting.storeVariant(compoundtag, this.getVariant());
+				CompoundTag tag = stack.getOrCreateTagElement("EntityTag");
+				Painting.storeVariant(tag, this.getVariant());
 				return this.spawnAtLocation(stack);
 			}
 		}
 
-		return this.spawnAtLocation(item);
+		stack.getOrCreateTagElement("EntityTag");
+		return this.spawnAtLocation(stack);
+	}
+
+	@Inject(method = "getPickResult", at = @At("RETURN"), cancellable = true)
+	public void getPickResult(CallbackInfoReturnable<ItemStack> cir) {
+		ItemStack stack = new ItemStack(Items.PAINTING);
+		CompoundTag tag = stack.getOrCreateTagElement("EntityTag");
+		Painting.storeVariant(tag, this.getVariant());
+		cir.setReturnValue(stack);
 	}
 }
