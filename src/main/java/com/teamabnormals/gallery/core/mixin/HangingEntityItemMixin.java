747930768dca092b9797773e0e1d8a8135961c62
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-01-06 16:16:40 -0600

    Fix a bug with vanilla paintings lol

---
+++ src/main/java/com/teamabnormals/gallery/core/mixin/HangingEntityItemMixin.java
@@ -0,0 +1,19 @@
+package com.teamabnormals.gallery.core.mixin;
+
+import net.minecraft.world.item.HangingEntityItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(HangingEntityItem.class)
+public abstract class HangingEntityItemMixin {
+
+	@Redirect(method = "useOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;shrink(I)V"))
+	public void getPickResult(ItemStack stack, int size, UseOnContext context) {
+		if (!context.getLevel().isClientSide()) {
+			stack.shrink(size);
+		}
+	}
+}
