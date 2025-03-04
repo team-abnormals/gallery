bfbd1d9694feba5ce3246d22c4ee8737db4573f4
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-03-03 22:28:08 -0600

    Update GalleryConfig.java

--- src/main/java/com/teamabnormals/gallery/core/GalleryConfig.java
+++ src/main/java/com/teamabnormals/gallery/core/GalleryConfig.java
@@ -25,7 +25,7 @@ public class GalleryConfig {
 			builder.push("painting_selector");
 			paintingSelector = builder.comment("If right-clicking with a Painting item opens up a Painting selector menu").define("Painting selector", true);
 			paintingSelectorRequiresCrouching = builder.comment("If you must be crouching to open the Painting selector").define("Painting selector requires crouching", false);
-			paintingSelectorOpensOnBlocks = builder.comment("If the Painting selector can be opened while right-clicking a block").define("Painting selector opens on blocks", false);
+			paintingSelectorOpensOnBlocks = builder.comment("If the Painting selector can be opened while right-clicking the top or bottom face of a block").define("Painting selector can open on blocks", true);
 			builder.pop();
 			builder.pop();
 		}
